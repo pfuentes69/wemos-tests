de2fa04dacbd8c73325880d7c2fc473df67c13f0
Pedro F <34808198+pfuentes69@users.noreply.github.com>
2023-03-01 23:36:06 +0100

    EST test

---
+++ wemos-est-test/README.md
@@ -0,0 +1,1 @@
+# wemos-awsiot-test
--- wemos-hcsr501-test/src/main.cpp
+++ wemos-hcsr501-test/src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 
 boolean active = false; //Declare intermediate variable 
-int pin = D4; // Set data pin
+int pin = D2; // Set data pin
 int movement = 0;
 
 void setup() {
   pinMode(pin, INPUT); // Set Pin7 as input 
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200); // Serial monitor at a baud rate of 115200
 }
 
 void loop() {
   movement = digitalRead(pin); // Read Sensor
+  digitalWrite(LED_BUILTIN, !movement);
   // a new movement was detected
+
   if(movement == HIGH && active == false) { 
     active = true;
     Serial.println("Motion detected");
